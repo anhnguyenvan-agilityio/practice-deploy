49ce12a553e20ed1527acb80712624914c5d8b6f
anhnguyenvan <anh.nguyenvan@asnet.com.vn>
2019-11-25 16:58:01 +0700

    .

--- openpixel.js
+++ openpixel.js
@@ -3,6 +3,7 @@
 ;(function(window, document, pixelFunc, pixelFuncName, pixelEndpoint, versionNumber) {
   "use strict";
   
+  console.log('======');
   function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
   
   function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
